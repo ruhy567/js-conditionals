395ad9a61a5f71aa2fb4a501d306d598354bd745
ruhy567 <galiba.chowdhury56@gmail.com>
2024-11-12 21:47:22 +0600

    all

---
+++ if.js
@@ -0,0 +1,32 @@
+if(13 < 10){
+
+    console.log('i will come');
+}
+
+var biriyaniprice = 250;
+if (biriyaniprice < 300){
+    console.log('khabo')
+}
+
+/*
+     if (condition){
+      execude code if above contion is true
+}
+      else{
+        execude code if the condition is false
+      }
+*/
+var weight = 40;
+if(weight < 20){
+    console.log('I will carry it by myself')
+}
+else{
+    console.log('I will rent a rickshaw')
+}
+var weight = 10;
+if(weight < 20){
+    console.log('I will carry it by myself')
+}
+else{
+    console.log('I will rent a rickshaw')
+}
