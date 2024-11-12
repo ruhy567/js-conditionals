80924e19aca354d86397c87b26df0ad9865e0657
ruhy567 <galiba.chowdhury56@gmail.com>
2024-11-12 21:24:52 +0600

    all

--- comparison.js
+++ comparison.js
@@ -1,3 +1,13 @@
 console.log(5 > 10);
 console.log(5 < 10);
 console.log(15 >= 10);
+
+var appleprice = 250;
+var peyaraPrice = 40;
+console.log (appleprice + peyaraPrice);
+console.log(appleprice > peyaraPrice);
+console.log(appleprice < peyaraPrice);
+console.log(10 == 10); // just maan check kortese 
+console.log(10 === '10'); // maan & type duitai check kortese 
+
+console.log(10 != '10'); // not equal
